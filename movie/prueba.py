4e8b1df8329da5a4971ffd016dae62c7714c18b4
Damian <damianduquel@gmail.com>
2022-09-07 23:52:18 -0500

    visualizacion peliculas

---
+++ movie/prueba.py
@@ -0,0 +1,6 @@
+nombre = input("INgrese su nombre: ")
+edad = int(input("Ingrese su edad"))
+estatura = float(input("Ingrese estatura"))
+
+misdatos = nombre + str(edad) + str(estatura)
+print(misdatos)
