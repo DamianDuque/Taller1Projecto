00c806e6a5991aa55d6a3cf07af3a05c57c650e0
Damian <damianduquel@gmail.com>
2022-09-08 00:11:01 -0500

    Formulario de busqueda

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Movie
-
 # Create your views here.
 
 def home(request):
@@ -10,11 +8,7 @@ def home(request):
     #return render(request, 'home.html')
     #return render(request, 'home.html', {'name:' 'Paola Vallejo'})
     searchTerm = request.GET.get('searchMovie')
-    if searchTerm:
-        movies = Movie.objects.filter(title__icontains= searchTerm)
-    else:
-        movies = Movie.objects.all()
-    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
+    return render(request, 'home.html', {'searchTerm':searchTerm})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
